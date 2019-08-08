1dfe13423b3164930d1523e6579d5d52a94cd973
selfworksbg@gmail.com <user@192.168.0.101>
2019-08-09 01:36:28 +0300

    update

--- sbin/download_from_git.sh
+++ sbin/download_from_git.sh
@@ -1,14 +1,26 @@
 #! /bin/bash
 
-if [ ! -d "/usr/share/wesellin" ]; then
-	mkdir /usr/share/wesellin
-fi
 
-chmod 775 -R "/usr/share/wesellin"
+rm -rf /usr/share/wesellin/latest
+mkdir -p /usr/share/wesellin/latest
 
-cd /usr/share/wesellin
+#if [ ! -d "/usr/share/wesellin/latest" ]; then
+#    mkdir /usr/share/wesellin/latest
+#fi
 
+chmod 775 -R "/usr/share/wesellin/latest"
+
+cd "/usr/share/wesellin/latest"
+
+#readlink -f "./"
+
+# Download latest version
+wget "http://download.wesellin.net/latest.zip" >> downloading.log
+
+# Unzip latest version
+unzip -o  "latest.zip" >> unziping.log
+
+# Remove zip
 unlink "latest.zip"
-wget "http://download.wesellin.net/latest.zip"
-unzip -o  "latest.zip"
 
+echo "Done!" 
