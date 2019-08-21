7c23fddab1ba632c5c3169fbe7768e003f2f10b1
Bozhidar Slaveykov <selfworksbg@gmail.com>
2019-08-21 08:40:31 +0300

    Update IndexController.php

--- plib/controllers/IndexController.php
+++ plib/controllers/IndexController.php
@@ -1,249 +1,289 @@
 <?php
 
-class IndexController extends pm_Controller_Action
-{
+class IndexController extends pm_Controller_Action {
 
-	protected $_accessLevel = [
-		'admin'
-	];
-	
-	protected $_moduleName = 'We Sell In';
-	
-	public function init()
-	{ 
-		parent::init();
-		
-		// Init tabs for all actions
-		$this->view->tabs = [
-			[
-				'title' => 'Domains',
-				'action' => 'index'
-			],
-			[
-			'title' => 'Install',
-				'action' => 'install'
-			],
-		    [
-		        'title' => 'Update',
-		        'action' => 'update'
-		    ],
-			[
-				'title' => 'Settings',
-				'action' => 'settings',
-			]
-		];
-	}
+    protected $_accessLevel = [
+        'admin'
+    ];
+    protected $_moduleName = 'We Sell In';
 
-	public function indexAction()
-	{
-		$this->view->pageTitle = $this->_moduleName . ' - Domains';
-		$this->view->list = $this->_getDomainsList();
-	}
-	
-	public function updateAction()
-	{
-	    $this->view->pageTitle = $this->_moduleName . ' - Update';
-	    
-	    $form = new pm_Form_Simple();
-	    
-	    $form->addControlButtons([
-	        'cancelLink' => pm_Context::getModulesListUrl(),
-	    ]);
-	    
-	    $this->view->form = $form;
-	}
-	
-	public function downloadAction() {
-	
-	   echo pm_ApiCli::callSbin('download_from_git.sh')['stdout'];
-           
-	   exit; 
-	}
-	
-	public function versionAction() {
-	  
-	    echo '1.0';
-	    exit;
-	}
-	
-	public function testAction()
-	{
+    public function init() {
+        parent::init();
+
+        // Init tabs for all actions
+        $this->view->tabs = [
+            [
+                'title' => 'Domains',
+                'action' => 'index'
+            ],
+            [
+                'title' => 'Install',
+                'action' => 'install'
+            ],
+            [
+                'title' => 'Update',
+                'action' => 'update'
+            ],
+            [
+                'title' => 'Settings',
+                'action' => 'settings',
+            ]
+        ];
+    }
+
+    public function indexAction() {
+        $this->view->pageTitle = $this->_moduleName . ' - Domains';
+        $this->view->list = $this->_getDomainsList();
+    }
+
+    public function updateAction() {
+
+        $versionOptions = array();
+        $releases = $this->_getReleases();
+        foreach ($releases as $release) {
+            $versionOptions[$release['file']] = $release['file'];
+        }
+        
+        $this->view->pageTitle = $this->_moduleName . ' - Update';
+
+        $form = new pm_Form_Simple();
+        $form->addElement('select', 'download_version', [
+            'label' => 'Version',
+            'multiOptions' => $versionOptions,
+            'required' => true,
+        ]);
+
+        $form->addControlButtons([
+            'cancelLink' => pm_Context::getModulesListUrl(),
+        ]);
+
+        $this->view->form = $form;
+
+        if ($this->getRequest()->isPost() && $form->isValid($this->getRequest()->getPost())) {
+
+            $downloadVersion = $form->getValue('download_version');
+            
+            foreach ($releases as $release) {
+                if ($release['file'] == $downloadVersion) {
+                    
+                    echo $downloadVersion;
+                    die();
+                    $downloadLog = pm_ApiCli::callSbin('download_from_git.sh',[base64_encode($release['download_url']), $downloadVersion])['stdout'];
+                }
+            }
+
+            echo $downloadLog;
+            die();
+            $this->_status->addMessage('info', 'Downloading version are started on queue!');
+            $this->_helper->json(['redirect' => pm_Context::getBaseUrl() . 'index.php/index/update']);
+        }
+    }
+
+    public function versionAction() {
+
+        echo '1.0';
+        exit;
+    }
+
+    public function testAction() {
         $newInstallation = new Modules_Wesellin_Install();
         $newInstallation->setDomainId(2);
         $newInstallation->setVersion(1);
         $newInstallation->setType('default');
         $newInstallation->run();
-	}
-	
-	public function installAction()
-	{
-		$this->view->pageTitle = $this->_moduleName . ' - Install';
-		
-		$domainsSelect = [];
-		foreach (pm_Domain::getAllDomains() as $domain) {
-			
-			$domainId = $domain->getId();
-			$domainName = $domain->getName();
-			
-			$domainsSelect[$domainId] = $domainName;
-		}
-		
-		$form = new pm_Form_Simple();
-		$form->addElement('select', 'installation_domain', [
-			'label' => 'Domain',
-			'multiOptions' => $domainsSelect,
-			'required' => true,
-		]);
-		$form->addElement('select', 'installation_version', [
-			'label' => 'Version',
-			'multiOptions' => ['1.0'=>'1.0'],
-			'required' => true,
-		]);
-		$form->addElement('radio', 'installation_type', [
-			'label' => 'Installation Type',
-			'multiOptions' =>
-			[
-				'default' => 'Default',
-				'sym_linked' => 'Sym-Linked'
-			],
-			'value' => pm_Settings::get('installation_type'),
-			'required' => true,
-		]);
-		
-		$form->addControlButtons([
-			'cancelLink' => pm_Context::getModulesListUrl(),
-		]);
-		
-		if ($this->getRequest()->isPost() && $form->isValid($this->getRequest()->getPost())) {
-			
-			$post = $this->getRequest()->getPost();
-			
-			$newInstallation = new Modules_Wesellin_Install();
-			$newInstallation->setDomainId($post['installation_domain']);
-			$newInstallation->setVersion(1);
-			$newInstallation->setType($post['installation_type']);
-			$newInstallation->run();
-		}
-		
-		$this->view->form = $form;
-	}
-	
-	public function settingsAction() {
-		
-		$this->view->pageTitle = $this->_moduleName . ' - Settings';
-		
-		$form = new pm_Form_Simple();
-		$form->addElement('radio', 'installation_settings', [
-			'label' => 'Installation Settings',
-			'multiOptions' => 
-				[
-					'auto' => 'Automaticlly install WeSellIn on new domains creation.', 
-					'manual' => 'Allow users to Manualy install WeSellIn from Plesk.',
-					'disabled'=> 'Disabled for all users'
-				],
-			'value' => pm_Settings::get('installation_settings'),
-			'required' => true,
-		]);
-		
-		$form->addElement('radio', 'installation_type', [
-			'label' => 'Installation Type',
-			'multiOptions' =>
-			[
-				'default' => 'Default',
-				'sym_linked' => 'Sym-Linked (saves a big amount of disk space)'
-			],
-			'value' => pm_Settings::get('installation_type'),
-			'required' => true,
-		]);
-		
-		
-		$form->addElement('select', 'installation_database_driver', [
-			'label' => 'Database Driver',
-			'multiOptions' => ['mysql' => 'MySQL', 'sqlite' => 'SQL Lite'],
-			'value' => pm_Settings::get('installation_database_driver'),
-			'required' => true,
-		]);
-		
-		$form->addControlButtons([
-			'cancelLink' => pm_Context::getModulesListUrl(),
-		]);
-		
-		if ($this->getRequest()->isPost() && $form->isValid($this->getRequest()->getPost())) {
-			
-			// Form proccessing
-			pm_Settings::set('installation_settings', $form->getValue('installation_settings'));
-			pm_Settings::set('installation_type', $form->getValue('installation_type'));
-			pm_Settings::set('installation_database_driver', $form->getValue('installation_database_driver'));
-			
-			$this->_status->addMessage('info', 'Settings was successfully saved.');
-			$this->_helper->json(['redirect' => pm_Context::getBaseUrl().'index.php/index/settings']);
-		}
-		
-		$this->view->form = $form;
-		
-	}
-	
-	private function _getDomainsList()
-	{
-		
-		$data = [];
-		$domains = pm_Domain::getAllDomains();
-		
-		$i = 0;
-		foreach ($domains as $domain) {
-			$domainDocumentRoot = $domain->getDocumentRoot();
-			$domainName = $domain->getName();
-			$domainIsActive = $domain->isActive();
-			$domainCreation = $domain->getProperty('cr_date');
-			
-			$data[$i] = [
-				'domain' => '<a href="#">'.$domainName.'</a>',
-				'created_date' => $domainCreation,
-				'type' => 'Symlink',
-				'document_root' => $domainDocumentRoot,
-				'active' => ($domainIsActive ? 'Yes' : 'No')
-			];
-			$i++;
-		}
-		
-		$options = [
-			'defaultSortField' => 'active',
-			'defaultSortDirection' => pm_View_List_Simple::SORT_DIR_DOWN,
-		];
-		$list = new pm_View_List_Simple($this->view, $this->_request, $options);
-		$list->setData($data);
-		$list->setColumns([
-			pm_View_List_Simple::COLUMN_SELECTION,
-			'domain' => [
-				'title' => 'Domain',
-				'noEscape' => true,
-				'searchable' => true,
-			],
-			'created_date' => [
-				'title' => 'Created at',
-				'noEscape' => true,
-				'searchable' => true,
-			],
-			'type' => [
-				'title' => 'Type',
-				'noEscape' => true,
-				'sortable' => false,
-			],
-			'active' => [
-				'title' => 'Active',
-				'noEscape' => true,
-				'sortable' => false,
-			],
-			'document_root' => [
-				'title' => 'Document Root',
-				'noEscape' => true,
-				'sortable' => false,
-			],
-		]);
-		
-		// Take into account listDataAction corresponds to the URL /list-data/
-		$list->setDataUrl(['action' => 'list-data']);
-		
-		return $list;
-	}
+    }
+
+    public function installAction() {
+
+        $this->view->pageTitle = $this->_moduleName . ' - Install';
+
+        $domainsSelect = [];
+        foreach (pm_Domain::getAllDomains() as $domain) {
+
+            $domainId = $domain->getId();
+            $domainName = $domain->getName();
+
+            $domainsSelect[$domainId] = $domainName;
+        }
+
+        $form = new pm_Form_Simple();
+        $form->addElement('select', 'installation_domain', [
+            'label' => 'Domain',
+            'multiOptions' => $domainsSelect,
+            'required' => true,
+        ]);
+        $form->addElement('select', 'installation_version', [
+            'label' => 'Version',
+            'multiOptions' => ['1.0' => '1.0'],
+            'required' => true,
+        ]);
+        $form->addElement('radio', 'installation_type', [
+            'label' => 'Installation Type',
+            'multiOptions' =>
+            [
+                'default' => 'Default',
+                'sym_linked' => 'Sym-Linked'
+            ],
+            'value' => pm_Settings::get('installation_type'),
+            'required' => true,
+        ]);
+
+        $form->addControlButtons([
+            'cancelLink' => pm_Context::getModulesListUrl(),
+        ]);
+
+        if ($this->getRequest()->isPost() && $form->isValid($this->getRequest()->getPost())) {
+
+            $post = $this->getRequest()->getPost();
+
+            $newInstallation = new Modules_Wesellin_Install();
+            $newInstallation->setDomainId($post['installation_domain']);
+            $newInstallation->setVersion(1);
+            $newInstallation->setType($post['installation_type']);
+            $newInstallation->run();
+        }
+
+        $this->view->form = $form;
+    }
+
+    public function settingsAction() {
+
+        $this->view->pageTitle = $this->_moduleName . ' - Settings';
+
+        $form = new pm_Form_Simple();
+        $form->addElement('radio', 'installation_settings', [
+            'label' => 'Installation Settings',
+            'multiOptions' =>
+            [
+                'auto' => 'Automaticlly install WeSellIn on new domains creation.',
+                'manual' => 'Allow users to Manualy install WeSellIn from Plesk.',
+                'disabled' => 'Disabled for all users'
+            ],
+            'value' => pm_Settings::get('installation_settings'),
+            'required' => true,
+        ]);
+
+        $form->addElement('radio', 'installation_type', [
+            'label' => 'Installation Type',
+            'multiOptions' =>
+            [
+                'default' => 'Default',
+                'sym_linked' => 'Sym-Linked (saves a big amount of disk space)'
+            ],
+            'value' => pm_Settings::get('installation_type'),
+            'required' => true,
+        ]);
+
+
+        $form->addElement('select', 'installation_database_driver', [
+            'label' => 'Database Driver',
+            'multiOptions' => ['mysql' => 'MySQL', 'sqlite' => 'SQL Lite'],
+            'value' => pm_Settings::get('installation_database_driver'),
+            'required' => true,
+        ]);
+
+        $form->addControlButtons([
+            'cancelLink' => pm_Context::getModulesListUrl(),
+        ]);
+
+        if ($this->getRequest()->isPost() && $form->isValid($this->getRequest()->getPost())) {
+
+            // Form proccessing
+            pm_Settings::set('installation_settings', $form->getValue('installation_settings'));
+            pm_Settings::set('installation_type', $form->getValue('installation_type'));
+            pm_Settings::set('installation_database_driver', $form->getValue('installation_database_driver'));
+
+            $this->_status->addMessage('info', 'Settings was successfully saved.');
+            $this->_helper->json(['redirect' => pm_Context::getBaseUrl() . 'index.php/index/settings']);
+        }
+
+        $this->view->form = $form;
+    }
+
+    private function _getDomainsList() {
+
+        $data = [];
+        $domains = pm_Domain::getAllDomains();
+
+        $i = 0;
+        foreach ($domains as $domain) {
+            $domainDocumentRoot = $domain->getDocumentRoot();
+            $domainName = $domain->getName();
+            $domainIsActive = $domain->isActive();
+            $domainCreation = $domain->getProperty('cr_date');
+
+            $data[$i] = [
+                'domain' => '<a href="#">' . $domainName . '</a>',
+                'created_date' => $domainCreation,
+                'type' => 'Symlink',
+                'document_root' => $domainDocumentRoot,
+                'active' => ($domainIsActive ? 'Yes' : 'No')
+            ];
+            $i++;
+        }
+
+        $options = [
+            'defaultSortField' => 'active',
+            'defaultSortDirection' => pm_View_List_Simple::SORT_DIR_DOWN,
+        ];
+        $list = new pm_View_List_Simple($this->view, $this->_request, $options);
+        $list->setData($data);
+        $list->setColumns([
+            pm_View_List_Simple::COLUMN_SELECTION,
+            'domain' => [
+                'title' => 'Domain',
+                'noEscape' => true,
+                'searchable' => true,
+            ],
+            'created_date' => [
+                'title' => 'Created at',
+                'noEscape' => true,
+                'searchable' => true,
+            ],
+            'type' => [
+                'title' => 'Type',
+                'noEscape' => true,
+                'sortable' => false,
+            ],
+            'active' => [
+                'title' => 'Active',
+                'noEscape' => true,
+                'sortable' => false,
+            ],
+            'document_root' => [
+                'title' => 'Document Root',
+                'noEscape' => true,
+                'sortable' => false,
+            ],
+        ]);
+
+        // Take into account listDataAction corresponds to the URL /list-data/
+        $list->setDataUrl(['action' => 'list-data']);
+
+        return $list;
+    }
+
+    private function _getReleases() {
+
+        $tuCurl = curl_init();
+        curl_setopt($tuCurl, CURLOPT_URL, 'download.wesellin.net/getVersions.php?secret=9704256060');
+        curl_setopt($tuCurl, CURLOPT_VERBOSE, 0);
+        curl_setopt($tuCurl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($tuCurl, CURLOPT_SSL_VERIFYPEER, false);
+
+        $tuData = curl_exec($tuCurl);
+        if (!curl_errno($tuCurl)) {
+            $info = curl_getinfo($tuCurl);
+            $debug = 'Took ' . $info['total_time'] . ' seconds to send a request to ' . $info['url'];
+        } else {
+            $debug = 'Curl error: ' . curl_error($tuCurl);
+        }
+
+        curl_close($tuCurl);
+
+        $json = json_decode($tuData, TRUE);
+
+        return $json;
+    }
+
 }
