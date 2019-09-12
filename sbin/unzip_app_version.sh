adc16750baa5cde74a2a9783ca22f852e178bab3
Bozhidar Slaveykov <50577633+bobimicroweber@users.noreply.github.com>
2019-09-12 18:30:15 +0300

    Update unzip_app_version.sh

--- sbin/unzip_app_version.sh
+++ sbin/unzip_app_version.sh
@@ -1,35 +1,35 @@
 #! /bin/bash
 
-downloadUrl=`echo $1 | base64 -d`
+downloadUrl=$(echo "$1" | base64 -d)
 
 downloadCacheFolder='/usr/share/'$2'-download-cache'
 
-if [ ! -d $downloadCacheFolder ]; then
-    mkdir $downloadCacheFolder
+if [ ! -d "$downloadCacheFolder" ]; then
+    mkdir "$downloadCacheFolder"
 fi
 
-cd $downloadCacheFolder
+cd "$downloadCacheFolder" || exit
 
 zipDownloadedFile=$2'-app-cache.zip';
 
 echo 'Download from url...'
-wget $downloadUrl -O $zipDownloadedFile
+wget "$downloadUrl" -O "$zipDownloadedFile"
 
 # Unzip selected version
 echo 'Unzip file...'
-unzip $zipDownloadedFile -d latest > unziping.log
+unzip "$zipDownloadedFile" -d latest > unziping.log
 
-if [ ! -d '/usr/share/'$2 ]; then
-	echo 'Make dir /usr/share/'$2
-    mkdir '/usr/share/'$2
+if [ ! -d '/usr/share/'"$2" ]; then
+	echo 'Make dir /usr/share/'"$2"
+    mkdir '/usr/share/'"$2"
 fi
 
-echo 'Delete files from /usr/share/'$2'/latest'
-rm -rf '/usr/share/'$2'/latest'
+echo 'Delete files from /usr/share/'"$2"'/latest'
+rm -rf '/usr/share/'"$2"'/latest'
 
-echo 'Move file to /usr/share/'$2
-mv latest /usr/share/$2
+echo 'Move file to /usr/share/'"$2"
+mv latest /usr/share/"$2"
 
-cd /usr/share/$2/latest
+cd /usr/share/"$2"/latest || exit
 
 echo "Done!"
